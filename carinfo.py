5c2d45bbb4d5e153cdd1a170545b17be92a390e1
jasonjin34 <hikingjack34@gmail.com>
2019-05-11 22:56:56 +0200

    trained xml

--- carinfo.py
+++ carinfo.py
@@ -4,5 +4,16 @@ fbg = open('bg.txt', 'w+')
 for i in range(550):
     f.write('pos/pos-' + str(i) + '.pgm 1 0 0 100 40\n')
 
+for i in range(8144):
+    temp_i = i + 1
+    temp_i_len = len(str(temp_i))
+    temp_str = ''
+    
+    for i in range(5 - temp_i_len):
+        temp_str = temp_str + '0'
+
+    temp_str = temp_str + str(temp_i)
+    f.write('pos/' + temp_str + '.jpg 1 0 0 100 40\n')
+
 for i in range(500):
     fbg.write('neg/neg-' + str(i) + '.pgm\n')
