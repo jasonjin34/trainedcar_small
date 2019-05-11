7e30887f9cebaddca2deca9b9b68583d49c3d664
jasonjin34 <hikingjack34@gmail.com>
2019-05-11 11:43:39 +0200

    init

---
+++ carinfo.py
@@ -0,0 +1,8 @@
+f = open('cars.info', 'w+')
+fbg = open('bg.txt', 'w+')
+
+for i in range(550):
+    f.write('pos/pos-' + str(i) + '.pgm 1 0 0 100 40\n')
+
+for i in range(500):
+    fbg.write('neg/neg-' + str(i) + '.pgm\n')
---
+++ sampletrain.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+opencv_createsamples -info cars.info -num 550 -w 48 -h 24 -vec cars.vec
+
+opencv_traincascade -data data -vec cars.vec -bg bg.txt -numPos 500 -numNeg 500 -numStages 20 -w 48 -h 24 -featureType LBP
